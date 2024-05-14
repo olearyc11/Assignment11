90548dfed25d2d6146831d70730493704b9e1415
olearyc11 <157394769+olearyc11@users.noreply.github.com>
2024-05-14 16:56:39 -0400

    2nd Commit

--- src/main/java/com/codercampus/Assignment11/domain/TransactionComparator.java
+++ src/main/java/com/codercampus/Assignment11/domain/TransactionComparator.java
@@ -1,16 +1,14 @@
 package com.codercampus.Assignment11.domain;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
+
 
 public class TransactionComparator implements Comparator<Transaction> {
 
-	
 	@Override
 	public int compare(Transaction o1, Transaction o2) {
-		
+
 		if (o1 == null && o2 == null) {
 			return 0;
 		} else if (o1 == null) {
@@ -18,25 +16,19 @@ public class TransactionComparator implements Comparator<Transaction> {
 		} else if (o2 == null) {
 			return -1;
 		}
-	
 
-        LocalDateTime date1 = o1.getDate();
-        LocalDateTime date2 = o2.getDate();
+		LocalDateTime date1 = o1.getDate();
+		LocalDateTime date2 = o2.getDate();
 
-        if (date1 == null && date2 == null) {
-            return 0;
-        } else if (date1 == null) {
-            return 1;
-        } else if (date2 == null) {
-            return -1;
-        }
+		if (date1 == null && date2 == null) {
+			return 0;
+		} else if (date1 == null) {
+			return 1;
+		} else if (date2 == null) {
+			return -1;
+		}
 
-        return date1.compareTo(date2);
+		return date1.compareTo(date2);
 	}
-	
-	  public List<Transaction> sortTransactions(List<Transaction> transactions) {
-	        Collections.sort(transactions, this);
-	        return transactions;
-	    }
 
 }
--- src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.codercampus.Assignment11.domain.Transaction;
@@ -18,8 +19,9 @@ public class TransactionController {
 	TransactionService transactionService = new TransactionService();
 	
 	@GetMapping("/transactions")
-	public String getAllTransactions() {
+	public String getAllTransactions(ModelMap model) {
 		List<Transaction> transactionList = transactionService.sortTransactionList();
+		model.put("transactions", transactionList);
 		return "transactions";
 	}
 	
